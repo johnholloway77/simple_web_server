b02028bdb202cc91aefcf82c666b86e61ac21327
johnholloway77 <john.holloway@edu.sait.ca>
2024-08-17 16:47:18 -0600

    Can set environment vars with cgiExe.c

--- simple_server/cgi/cgiExe.c
+++ simple_server/cgi/cgiExe.c
@@ -1,4 +1,148 @@
 
+// #include <limits.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+// #include <sys/types.h>
+// #include <sys/wait.h>
+
+// #define BUFFER 1024
+// #define CGI_BIN_DIR "./cgi-bin/"
+
+// char *cgiExe(char *cgiURI, char **args) {
+
+//     char *file_name = strtok(cgiURI, "?");
+
+//     printf("\tfile name: %s\n", file_name);
+
+//     char *param_string = strtok(NULL, "?");
+
+//     printf("\tparam_string = %s\n", param_string);
+
+//     char *buffer;
+//     char *name;
+//     char *val;
+
+
+
+
+//     int pipe_stdin[2];
+//     int pipe_stdout[2];
+//     pid_t pid;
+
+//     if (pipe(pipe_stdin) == -1 || pipe(pipe_stdout) == -1) {
+//         return "HTTP/1.0 500 Internal Error\r\n"
+//                "Content-Type: text/plain\r\n"
+//                "Connection: close\r\n\r\n"
+//                "Error: creating pipes";
+//     }
+
+//     pid = fork();
+//     if (pid == -1) {
+//         return "HTTP/1.0 500 Internal Error\r\n"
+//                "Content-Type: text/plain\r\n"
+//                "Connection: close\r\n\r\n"
+//                "Error: fork()";
+//     }
+
+//     // Child process
+//     if (pid == 0) {
+//         //dup2(pipe_stdin[0], STDIN_FILENO);
+//         dup2(pipe_stdout[1], STDOUT_FILENO);  // Redirect stdout to pipe
+
+//         close(pipe_stdout[0]);  // Close unused read end
+//         close(pipe_stdin[1]);   // Close unused write end
+
+//         //set environment variables
+//         while((buffer = strtok(param_string, "&")) != NULL){
+//             param_string = NULL;
+
+//             name = strtok(buffer, "=");
+//             val = strtok(NULL, "=");
+
+//             printf("\tname %s value %s\n", name, val);
+
+//             setenv(name, val, 1);
+//         }
+
+
+//         char path[PATH_MAX];
+//         snprintf(path, PATH_MAX, "%s%s", CGI_BIN_DIR, file_name);
+
+//         printf("\tpathname: %s\n", path);
+//         char *exec_args[] = {path, NULL};
+
+
+
+//         if (execvp(path, exec_args) == -1) {
+//             perror("execvp failed");
+//             exit(EXIT_FAILURE);  // Exit on execvp failure
+//         }
+
+//         exit(EXIT_SUCCESS);  // Not reached if execvp is successful
+//     }
+
+//     // Parent process
+//     else {
+//         close(pipe_stdout[1]);  // keep both stdin and stdout open to cgi process
+
+//         close(pipe_stdin[0]);   // Close unused read end
+//         close(pipe_stdin[1]);   // Close unused write end
+
+//         size_t nread;
+//         char buffer[BUFFER];
+//         char *response = malloc(BUFFER);
+//         size_t total_read = 0;
+
+//         if (response == NULL) {
+//             return "HTTP/1.0 500 Internal Error\r\n"
+//                    "Content-Type: text/plain\r\n"
+//                    "Connection: close\r\n\r\n"
+//                    "Error: malloc()";
+//         }
+
+//         while ((nread = read(pipe_stdout[0], buffer, BUFFER)) > 0) {
+//             response = realloc(response, total_read + nread + 1);
+//             if (response == NULL) {
+//                 return "HTTP/1.0 500 Internal Error\r\n"
+//                        "Content-Type: text/plain\r\n"
+//                        "Connection: close\r\n\r\n"
+//                        "Error: realloc()";
+//             }
+
+//             memcpy(response + total_read, buffer, nread);
+//             total_read += nread;
+//         }
+
+//         if (nread == -1) {
+//             free(response);
+//             return "HTTP/1.0 500 Internal Error\r\n"
+//                    "Content-Type: text/plain\r\n"
+//                    "Connection: close\r\n\r\n"
+//                    "Error: read() failed";
+//         }
+
+//         response[total_read] = '\0';  // Null-terminate the response
+//         close(pipe_stdout[0]);  // Close the read end of the pipe
+
+//         int status;
+//         waitpid(pid, &status, 0);
+
+//         // Check if the child process terminated successfully
+//         if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+//             free(response);
+//             return "HTTP/1.0 404 Not Found\r\n"
+//                    "Content-Type: text/html\r\n"
+//                    "Connection: close\r\n\r\n"
+//                    "<html><body><h1>404 Not Found</h1>"
+//                    "<p>The requested CGI script was not found or failed to execute.</p></body></html>\r\n";
+//         }
+
+//         return response;
+//     }
+// }
+
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,11 +150,49 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 #define BUFFER 1024
 #define CGI_BIN_DIR "./cgi-bin/"
 
-char *cgiExe(const char *file, char **args) {
+// Helper function to decode URL-encoded strings
+void url_decode(char *dst, const char *src) {
+    char a, b;
+    while (*src) {
+        if ((*src == '%') && ((a = src[1]) && (b = src[2])) && (isxdigit(a) && isxdigit(b))) {
+            if (a >= 'a') a -= 'a' - 'A';
+            if (a >= 'A') a -= ('A' - 10);
+            else a -= '0';
+            if (b >= 'a') b -= 'a' - 'A';
+            if (b >= 'A') b -= ('A' - 10);
+            else b -= '0';
+            *dst++ = 16 * a + b;
+            src += 3;
+        } else if (*src == '+') {
+            *dst++ = ' ';
+            src++;
+        } else {
+            *dst++ = *src++;
+        }
+    }
+    *dst = '\0';
+}
+
+char *cgiExe(char *cgiURI, char **args) {
+
+    char *file_name = strtok(cgiURI, "?");
+   // printf("\tfile name: %s\n", file_name);
+
+    char *file_name2 = strdup(file_name);
+    //printf("\tfile name2: %s\n", file_name2);
+
+    char *param_string = strtok(NULL, "?");
+    //printf("\tparam_string = %s\n", param_string);
+
+    char *buffer;
+    char *name;
+    char *val;
+
     int pipe_stdin[2];
     int pipe_stdout[2];
     pid_t pid;
@@ -32,15 +214,41 @@ char *cgiExe(const char *file, char **args) {
 
     // Child process
     if (pid == 0) {
-        //dup2(pipe_stdin[0], STDIN_FILENO);
         dup2(pipe_stdout[1], STDOUT_FILENO);  // Redirect stdout to pipe
 
         close(pipe_stdout[0]);  // Close unused read end
         close(pipe_stdin[1]);   // Close unused write end
 
-        char path[PATH_MAX];
-        snprintf(path, PATH_MAX, "%s%s", CGI_BIN_DIR, file);
+        // Set environment variables
+        while ((buffer = strtok(param_string, "&")) != NULL) {
+            param_string = NULL;  // Continue tokenizing the original string
 
+            name = strtok(buffer, "=");
+            val = strtok(NULL, "=");
+
+            if (name && val) {
+                char decoded_name[256];
+                char decoded_val[256];
+                url_decode(decoded_name, name);
+                url_decode(decoded_val, val);
+
+               // printf("\tname: %s value: %s\n", decoded_name, decoded_val);
+
+                char *s = decoded_name;
+                while(*s){
+                    *s = toupper(*s);
+                    s++;
+                }
+
+                setenv(decoded_name, decoded_val, 1);
+            }
+        }
+
+        char path[PATH_MAX];
+        snprintf(path, PATH_MAX, "%s%s", CGI_BIN_DIR, file_name2);
+       // printf("\tfile_name: %s\n", file_name2);
+
+        //printf("\tpathname: %s\n", path);
         char *exec_args[] = {path, NULL};
 
         if (execvp(path, exec_args) == -1) {
@@ -53,8 +261,7 @@ char *cgiExe(const char *file, char **args) {
 
     // Parent process
     else {
-        close(pipe_stdout[1]);  // keep both stdin and stdout open to cgi process
-
+        close(pipe_stdout[1]);  // Close unused write end
         close(pipe_stdin[0]);   // Close unused read end
         close(pipe_stdin[1]);   // Close unused write end
 
--- simple_server/setup_dir.sh
+++ simple_server/setup_dir.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Create the cgi-bin directory if it doesn't exist
-mkdir -p ./cgi-bin
+mkdir -p ./cgi-bin ./cgi-data
 
 # Write the C code into a file called helloWorld.c inside the cgi-bin directory
 cat <<EOF > cgi-bin/helloWorld.c
@@ -19,8 +19,86 @@ int main(int argc, char **argv) {
 }
 EOF
 
+cat <<EOF> cgi-bin/guestBook.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+
+  FILE *fp;
+  const char *filename = "cgi-data/guestbook.txt";
+  char name_buf[256];
+
+  char *name = getenv("NAME");
+
+  if(name == NULL ){
+      printf("HTTP/1.0 400 Bad Request\r\n"
+             "Content-Type: text/html\r\n"
+             "Connection: close\r\n"
+             "\r\n"
+             "<html><body><h1>Bad Request</h1><p>Name not provided</p></body></html>");
+      return 0;
+  }
+
+if(*name != '\0') {
+  fp = fopen(filename, "a");
+  if (fp == NULL) {
+    fp = fopen(filename, "w");
+    if (fp == NULL) {
+      printf("HTTP/1.0 500 Server Error\r\n"
+             "Content-Type: text/html\r\n"
+             "Connection: close\r\n"
+             "\r\n"
+             "<http><body><h1>Internal Server Error</h1><p>Could not get "
+             "guestbook</p></body></html>");
+      return 0;
+    }
+
+
+  }
+
+  fprintf(fp, "%s\n", name);
+
+  fclose(fp);
+}
+
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    printf("HTTP/1.0 500 Server Error\r\n"
+           "Content-Type: text/html\r\n"
+           "Connection: close\r\n"
+           "\r\n"
+           "<http><body><h1>Internal Server Error</h1><p>Could not get "
+           "guestbook</p></body></html>");
+    return 0;
+  }
+
+  printf("HTTP/1.0 200 OK\r\n"
+         "Content-Type: text/html\r\n"
+         "Connection: close\r\n"
+         "\r\n"
+         "<html><body><h1>Guest Book</h1><p>The following people have signed "
+         "the guest book:</p>");
+
+  while (fgets(name_buf, sizeof(name_buf), fp) != NULL) {
+    printf("<p>Visitor: %s</p>", name_buf);
+  }
+
+  printf("</body></html>");
+
+  fclose(fp);
+
+  return 0;
+}
+
+EOF
+
+
 # Compile the C program into an executable called helloWorld inside the cgi-bin directory
-cc cgi-bin/helloWorld.c -o cgi-bin/helloWorld
+cc cgi-bin/helloWorld.c -o cgi-bin/helloWorld.cgi
+cc cgi-bin/guestBook.c -o cgi-bin/guestBook.cgi
 
 # Make an index.html page for the root directory
 cat <<EOF > index.html
@@ -35,6 +113,16 @@ that speaks a limited version of HTTP/1.0 as defined in RFC1945. For more inform
 <a href="https://stevens.netmeister.org/631/f23-group-project.html">assignment page.</a></p>
 
 <p>This program is made simply for me to learn more about interprocess communication and sockets.</p>
+
+<br>
+<a href="./cgi-bin/helloWorld.cgi">Click here for a CGI helloWorld page</a><br>
+<br>
+<h2>Sign the guestbook!</h2>
+<form  action="./cgi-bin/guestBook.cgi">
+<label for="name">Name: </label>
+<input type="text" id="name" name="NAME">
+<input type="submit" value="submit">
+</form>
 
 </body>
 </html>
