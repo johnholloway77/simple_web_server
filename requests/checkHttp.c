261ba83d5e59ac7fb38480c3b33f6635a5294767
John Holloway <mail@jholloway.dev>
2026-01-24 22:42:34 -0700

    formatting

--- requests/checkHttp.c
+++ requests/checkHttp.c
@@ -3,11 +3,11 @@
 int
 checkHttp(const char *http_str)
 {
-  if(strcmp(http_str, "HTTP/1.0") == 0)
-    return 1;
+	if (strcmp(http_str, "HTTP/1.0") == 0)
+		return 1;
 
-  if(strcmp(http_str, "HTTP/1.1") == 0)
-    return 1;
+	if (strcmp(http_str, "HTTP/1.1") == 0)
+		return 1;
 
-  return 0;
+	return 0;
 }
--- sockets/handleSocket.c
+++ sockets/handleSocket.c
@@ -9,38 +9,39 @@
 void
 handleSocket(int sock, enum sockType sockType, magic_t magic)
 {
-  int fd;
-  pid_t pid;
-  socklen_t length;
+	int fd;
+	pid_t pid;
+	socklen_t length;
 
-  // I'm using a union as an excuse to practice with them and learn more.
-  union sockaddr_union client;
+	// I'm using a union as an excuse to practice with them and learn more.
+	union sockaddr_union client;
 
-  length = (sockType == TYPE_SOCK_V4) ? sizeof(client.client_v4)
-                                      : sizeof(client.client_v6);
+	length = (sockType == TYPE_SOCK_V4) ? sizeof(client.client_v4)
+					    : sizeof(client.client_v6);
 
-  if((fd = accept(sock,
-                  (struct sockaddr *) (sockType == TYPE_SOCK_V4
-                                           ? (void *) &client.client_v4
-                                           : (void *) &client.client_v6),
-                  &length)) < 0)
-    {
-      perror("accept");
-      return; // -1;
-    }
+	if ((fd = accept(
+		     sock,
+		     (struct sockaddr *)(sockType == TYPE_SOCK_V4
+						 ? (void *)&client.client_v4
+						 : (void *)&client.client_v6),
+		     &length)) < 0)
+	{
+		perror("accept");
+		return; // -1;
+	}
 
-  if((pid = fork()) < 0)
-    {
-      perror("fork");
-      exit(EXIT_FAILURE);
-    }
-  else if(!pid)
-    {
-      handleConnection(fd, &client, sockType, magic);
-    }
-  else
-    {
-      // if parent close fd
-      (void) close(fd);
-    }
+	if ((pid = fork()) < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (!pid)
+	{
+		handleConnection(fd, &client, sockType, magic);
+	}
+	else
+	{
+		// if parent close fd
+		(void)close(fd);
+	}
 };
