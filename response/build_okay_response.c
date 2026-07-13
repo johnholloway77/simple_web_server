00d3d0c218610b5cb5529163f2ae699a98caa72b
johnholloway77 <jghollow@ucalgary.ca>
2026-07-13 09:41:27 -0600

    Pretty SVG

--- response/build_okay_response.c
+++ response/build_okay_response.c
@@ -23,13 +23,16 @@
 #define HAVE_DIRENT_D_TYPE 0
 #endif
 
-#define DIR_ICON                                                                \
-	"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" " \
-	"viewBox=\"0 0 16 16\"><path d=\"M1 3h5l2 2h7v8H1z\"/></svg>"
+#define DIR_ICON                                                                                                                                   \
+	"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-folder\" viewBox=\"0 0 16 16\">" \
+	"<path d=\"M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139q.323-.119.684-.12h5.396z\"/></svg>"
 
-#define FILE_ICON                                                               \
-	"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" " \
-	"viewBox=\"0 0 16 16\"><path d=\"M3 1h7l3 3v11H3z\"/></svg>"
+#define FILE_ICON                                                                                                                                                      \
+	"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-file-earmark\" viewBox=\"0 0 16 16\">"               \
+	"<path d=\"M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z\"/>" \
+	"</svg>"
+
+#define MAX_ICON_LEN strlen(DIR_ICON)
 
 static int
 check_resolve_args(const Client *c, const ResolvedPath *rp, const Request *req)
@@ -103,8 +106,14 @@ build_response_dir(Client *c, ResolvedPath *rp, Request *req)
 	if (count) {
 		// To do!
 		rewinddir(dir);
+
 		size_t buf_capacity = HTML_STRING_LEN + dir_path_len +
 				      (PATH_MAX + 64) * count;
+
+#ifdef HAVE_DIRENT_D_TYPE
+		buf_capacity += MAX_ICON_LEN;
+#endif
+
 		response_body = (char *)malloc(buf_capacity);
 
 		if (!response_body) {
