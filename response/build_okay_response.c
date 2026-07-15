81f2c62abd755d94a390bc51e786730988725cc2
johnholloway77 <jghollow@ucalgary.ca>
2026-07-15 16:10:29 -0600

    build_response_dir() to us open_memstream()
    Signed-off-by: johnholloway77 <jghollow@ucalgary.ca>

--- response/build_okay_response.c
+++ response/build_okay_response.c
@@ -25,7 +25,10 @@
 
 #define START_HTML_STRING "<html><body><h1>Directory:</h1><ul>"
 #define END_HTML_STRING "</ul></body></html>"
-#define HTML_STRING_LEN strlen(START_HTML_STRING) + strlen(END_HTML_STRING)
+#define HTML_STRING_LEN (strlen(START_HTML_STRING)) + (strlen(END_HTML_STRING))
+
+#define EMPTY_DIR_HTML "<h2>Empty directory</h2>"
+#define EMPTY_DIR_LEN strlen(EMPTY_DIR_HTML)
 
 #if defined(__FreeBSD__) || defined(__APPLE__) || defined(__linux__)
 #define HAVE_DIRENT_D_TYPE 1
@@ -41,8 +44,6 @@
 	"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-file-earmark\" viewBox=\"0 0 16 16\">"               \
 	"<path d=\"M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z\"/>" \
 	"</svg>"
-
-#define MAX_ICON_LEN strlen(DIR_ICON)
 
 extern const uint32_t app_flags;
 
@@ -272,155 +273,124 @@ build_response_dir(Client *c, ResolvedPath *rp, Request *req)
 	}
 
 	char *dir_path = req->path + 1;
-	unsigned long dir_path_len = strlen(dir_path);
 
 	DBG("dir path: %s\n", dir_path);
 
 	DIR *dir = NULL;
 	struct dirent *dirp;
-	char *response_body;
-	int response_body_on_heap =
-	    0; // to be used when dynamically allocating response
-
-	char header[MAX_HEADER_BUF];
-	int header_len;
 
 	dir = opendir(dir_path);
 	if (!dir) {
 		DBG("Dir null!\n");
+		c->resp_val = RESP_500;
+		return -1;
+	}
+
+	char *dir_list;
+	size_t dir_list_len = 0;
+	size_t local_body_len = 0;
+
+	FILE *body_buffer = open_memstream(&dir_list, &dir_list_len);
+	if (!body_buffer) {
 		closedir(dir);
 		c->resp_val = RESP_500;
 		return -1;
 	}
 
-	uint32_t count = 0;
 	while ((dirp = readdir(dir)) != NULL) {
-		if (dirp->d_name[0] != '.') {
-			count++;
+		if (dirp->d_name[0] == '.') {
+			continue;
 		}
-	}
-
-	if (count) {
-		// To do!
-		rewinddir(dir);
-
-		size_t buf_capacity = HTML_STRING_LEN + dir_path_len +
-				      (PATH_MAX + 64) * count;
-
-#ifdef HAVE_DIRENT_D_TYPE
-		buf_capacity += MAX_ICON_LEN;
-#endif
-
-		response_body = (char *)malloc(buf_capacity);
-
-		if (!response_body) {
-			DBG("Error allocating dynamic response body\n");
-			closedir(dir);
-			c->resp_val = RESP_500;
-			return -1;
-		}
-
-		response_body_on_heap = 1;
-
-		unsigned long offset = 0;
-
-		offset += snprintf(response_body,
-		    buf_capacity - offset,
-		    START_HTML_STRING);
-
-		while ((dirp = readdir(dir)) != NULL) {
-			if (dirp->d_name[0] == '.') {
-				continue;
-			}
 
 #if HAVE_DIRENT_D_TYPE
-			const char *icon = (dirp->d_type == DT_DIR) ? DIR_ICON
-								    : FILE_ICON;
+		const char *icon = (dirp->d_type == DT_DIR) ? DIR_ICON
+							    : FILE_ICON;
 #else
-			const char *icon = "";
+		const char *icon = "";
 #endif
 
-			offset += snprintf(response_body + offset,
-			    buf_capacity - offset,
-			    "<li>%s <a href=\"/%s/%s\">%s</a></li>",
-			    icon,
-			    dir_path,
-			    dirp->d_name,
-			    dirp->d_name);
-		}
-
-		offset += snprintf(response_body + offset,
-		    buf_capacity - offset,
-		    END_HTML_STRING);
+		fprintf(body_buffer,
+		    "<li>%s <a href=\"/%s/%s\">%s</a></li>",
+		    icon,
+		    dir_path,
+		    dirp->d_name,
+		    dirp->d_name);
 	}
-	else {
-		response_body =
-		    "<html><body><h2>Empty directory</h2></body></html>";
-	}
-
-	c->body_len = strlen(response_body);
 
 	struct tm *utc_time = gmtime(&rp->last_mod);
+	if (!utc_time) {
+		fclose(body_buffer);
+		free(dir_list);
+		closedir(dir);
+		c->resp_val = RESP_500;
+		return -1;
+	}
+
 	char mod_time_buf[32];
 	strftime(mod_time_buf,
 	    sizeof(mod_time_buf),
 	    "%Y-%m-%dT%H:%M:%SZ",
 	    utc_time);
 
-	header_len = snprintf(header,
-	    MAX_HEADER_BUF,
+	FILE *out_stream = open_memstream(&c->out_buf, &c->output_length);
+	if (!out_stream) {
+		fclose(body_buffer);
+		free(dir_list);
+		closedir(dir);
+		c->resp_val = RESP_500;
+		return -1;
+	}
+
+	if (dir_list_len > 0) {
+		local_body_len = dir_list_len + HTML_STRING_LEN;
+	}
+	else {
+		local_body_len = HTML_STRING_LEN + EMPTY_DIR_LEN;
+	}
+
+	fprintf(out_stream,
 	    "HTTP/1.0 200 OK\r\n"
 	    "Date: %s\r\n"
 	    "Server: %s\r\n"
 	    "Last-Modified: %s\r\n"
-	    "Content-Type: text/HTML\r\n"
+	    "Content-Type: text/html\r\n"
 	    "Content-Length: %zu\r\n"
 	    "Connection: close\r\n"
 	    "\r\n",
 	    req->time_received,
 	    SERVER_VERSION,
 	    mod_time_buf,
-	    c->body_len);
+	    local_body_len);
+	fflush(out_stream);
 
-	DBG("# of files in dir: %u\n", count);
+	c->header_len = c->output_length;
 
 	if (HTTP_HEAD == req->method) {
-		c->out_buf = malloc(header_len + 1);
-		if (!c->out_buf) {
-			perror("error build response file: ");
-			c->resp_val = RESP_500;
-			return -1;
-		}
-		memcpy(c->out_buf, header, header_len);
-		c->output_length = header_len;
-
-		if (response_body_on_heap) {
-			free(response_body);
-		}
-
+		fclose(out_stream);
+		fclose(body_buffer);
+		free(dir_list);
 		closedir(dir);
+
+		c->header_len = c->output_length;
 		return 0;
 	}
 
-	c->out_buf = malloc(header_len + c->body_len + 1);
-	if (!c->out_buf) {
-		perror("error build response file: ");
-		closedir(dir);
-		c->resp_val = RESP_500;
-		return -1;
+	fprintf(out_stream, START_HTML_STRING);
+	if (dir_list_len) {
+		fwrite(dir_list, 1, dir_list_len, out_stream);
 	}
-
-	memcpy(c->out_buf, header, header_len);
-	memcpy(c->out_buf + header_len, response_body, c->body_len);
-
-	c->output_length = header_len + c->body_len;
-
-	// free dynamic response buffer if created
-	if (response_body_on_heap) {
-		free(response_body);
+	else {
+		fprintf(out_stream, EMPTY_DIR_HTML);
 	}
+	fprintf(out_stream, END_HTML_STRING);
 
+	fclose(out_stream);
+	fclose(body_buffer);
+	free(dir_list);
 	closedir(dir);
+
+	c->body_len = c->output_length - c->header_len;
+
 	return 0;
 }
 
@@ -432,16 +402,25 @@ build_response_file(Client *c, ResolvedPath *rp, Request *req)
 		return -1;
 	}
 
-	char header[MAX_HEADER_BUF];
 	struct tm *utc_time = gmtime(&rp->last_mod);
+	if (!utc_time) {
+		c->resp_val = RESP_500;
+		return -1;
+	}
+
 	char mod_time_buf[32];
 	strftime(mod_time_buf,
 	    sizeof(mod_time_buf),
 	    "%Y-%m-%dT%H:%M:%SZ",
 	    utc_time);
 
-	int header_len = snprintf(header,
-	    MAX_HEADER_BUF,
+	FILE *out_stream = open_memstream(&c->out_buf, &c->output_length);
+	if (out_stream == NULL) {
+		DBG("Error creating out_stream - File response\n");
+		return -1;
+	}
+
+	fprintf(out_stream,
 	    "HTTP/1.0 200 OK\r\n"
 	    "Date: %s\r\n"
 	    "Server: %s\r\n"
@@ -456,40 +435,29 @@ build_response_file(Client *c, ResolvedPath *rp, Request *req)
 	    rp->mime_type,
 	    rp->file_size);
 
-	if (HTTP_HEAD == req->method) {
-		c->out_buf = malloc(header_len + 1);
-		if (!c->out_buf) {
-			perror("error build response file: ");
+	fflush(out_stream);
+	c->header_len = c->output_length;
 
-			c->resp_val = RESP_500;
-			return -1;
-		}
-		memcpy(c->out_buf, header, header_len);
-		c->output_length = header_len;
-		c->body_len = c->file_size;
-
-		return 0;
-	}
-
-	DBG("Header:\n%s\n", header);
-
+	DBG("Header:\n%s\n", c->out_buf);
 	DBG("Client info:\n"
 	    "c->file_size = %zu\n",
 	    c->file_size);
 
-	c->out_buf = malloc(header_len + c->file_size + 1);
-	if (!c->out_buf) {
-		perror("error build response file: ");
-		c->resp_val = RESP_500;
-		return -1;
+	if (HTTP_HEAD == req->method) {
+		c->body_len = c->file_size;
+		fclose(out_stream);
+		return 0;
 	}
 
-	memcpy(c->out_buf, header, header_len);
+	char buf[8192];
+	size_t n;
 
-	size_t n = fread(c->out_buf + header_len, 1, c->file_size, c->file_ptr);
+	while ((n = fread(buf, 1, sizeof buf, rp->file_ptr)) > 0) {
+		fwrite(buf, 1, n, out_stream);
+	}
 
-	c->output_length = header_len + n;
-	c->body_len = c->file_size;
+	fclose(out_stream);
+	c->body_len = c->output_length - c->header_len;
 
 	return 0;
 }
